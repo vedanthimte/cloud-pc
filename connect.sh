d98896bc92d277b0ee1cb3fcfb5dc83f8e0d34e4
Vedant Himte <112320570+vedanthimte@users.noreply.github.com>
2023-10-01 10:13:29 +0530

    Create connect.sh script
    This Script Will connect to private network

---
+++ connect.sh
@@ -0,0 +1,14 @@
+# VPS with gui and interconnected network
+# Script Autor : VEDANT HIMTE
+#Here i am using zerotier service i you want to use add your network id .
+
+curl -s https://install.zerotier.com | sudo bash
+
+sudo echo 'alias start="sudo service zerotier-one restart"' >> /root/.bashrc
+sudo echo 'alias connect="sudo zerotier-cli join 363c67c55ac194ca"' >> /root/.bashrc
+
+sudo echo 'alias start="sudo service zerotier-one restart"' >> /home/codespace/.bashrc
+sudo echo 'alias connect="sudo zerotier-cli join 363c67c55ac194ca"' >> /home/codespace/.bashrc
+
+sudo service zerotier-one restart
+sudo zerotier-cli join 363c67c55ac194ca
